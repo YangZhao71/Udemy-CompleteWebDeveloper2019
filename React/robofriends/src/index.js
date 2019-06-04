1373dda7b24eb0c4129760fb9951361436d0c6bc
Yangmengyuan Zhao <zymy701@gmail.com>
2019-06-04 17:18:54 +0800

    React App 5

--- React/robofriends/src/index.js
+++ React/robofriends/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // if no signature, assume it as Javascript file
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
